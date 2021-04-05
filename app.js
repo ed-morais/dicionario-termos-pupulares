a359fe8f9c0fed6027f463f6fe5c47076c96d274
Edilson Morais <61170661+ed-morais@users.noreply.github.com>
2021-04-05 17:09:11 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelector(".nav-links li");
-
-hamburguer.addEventListener("click", () => {
-    navLinks.classList.toggle("open")
-})
